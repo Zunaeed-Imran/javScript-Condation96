c5f0005dc9deee9e7b9df978ebb19548f26328ee
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-12 17:01:53 +0600

    Add files via upload

---
+++ JS-condation.js
@@ -0,0 +1,14 @@
+let age = 10;
+
+
+
+if (age < 18) {
+  console.log('YOu are Young!');
+} else if (age ==18) {
+  console.log('Welcome adult!');
+} else if (age ==18 && age < 30) {
+  console.log('you are mature')
+}
+else {
+  console.log('you are Old!');
+}
